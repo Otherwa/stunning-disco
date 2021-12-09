f6374a79f882f3ccaba4db8f51cd412e8805ba80
Devashish Sule <90174329+devashishSule@users.noreply.github.com>
2021-12-09 17:18:04 +0530

    Add files via upload

---
+++ javascript/init.js
@@ -0,0 +1,17 @@
+//timer
+var countDownDate = new Date("Jan 1, 2022 00:00:00").getTime(); //Set date on event
+// Update 1 second
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var timerem = countDownDate - now;
+    // days, hours, minutes and seconds
+    var days = Math.floor(timerem / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timerem % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timerem % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timerem % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = days + "d " + hours + "hrs " + minutes + "min " + seconds + "sec ";
+    if (timerem < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "FINISHED?";
+    }
+}, 1000);
