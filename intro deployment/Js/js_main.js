828bad60d76e7cbadfca3f0201094caa89f2f47f
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2021-12-12 17:31:36 +0530

    Add files via upload
    beta 2 commit

---
+++ intro deployment/Js/js_main.js
@@ -0,0 +1,21 @@
+//timer
+var countDownDate = new Date("Dec 25, 2021 15:37:25").getTime(); //Set date on event
+// Update 1 second
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var timerem = countDownDate - now;
+    // days, hours, minutes and seconds
+  var days = Math.floor(timerem / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timerem % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timerem % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timerem % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + "d " + hours + "hrs "+ minutes + "min " + seconds + "sec ";
+  if (timerem < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "FINISHED ?";
+  }
+}, 1000);
+
+function Redirect(){
+  window.open('http://stackoverflow.com', '_blank');
+}
